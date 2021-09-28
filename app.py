684014bc44351847ef2963b75639ebceadcf0abd
Issac Abraham <issaca32@gmail.com>
2021-09-28 18:28:43 +0100

    gets all Tasks for a user

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_mysqldb import MySQL
 from flask_restful import Api
 import yaml
 from endpoints.User import User
+from endpoints.TaskList import TaskList
+from endpoints.Task import Task
 app = Flask(__name__)
 api = Api(app)
 db = yaml.load(open("endpoints/config.yaml"))
@@ -14,6 +16,8 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 api.add_resource(User, '/api/v1/user/<string:email>', endpoint='get-user', methods=['GET'], resource_class_kwargs={"mysql": mysql})
 api.add_resource(User, '/api/v1/user/create', endpoint='create-user', methods=['POST'], resource_class_kwargs={"mysql": mysql})
+api.add_resource(TaskList, '/api/v1/task/<string:user_id>/all', endpoint='get-tasklist', methods=['GET'], resource_class_kwargs={"mysql": mysql})
+api.add_resource(Task, '/api/v1/task/<string:user_id>/all', endpoint='get-tasklist', methods=['GET'], resource_class_kwargs={"mysql": mysql})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ endpoints/TaskList.py
@@ -0,0 +1,19 @@
+import yaml
+from flask_restful import Resource
+from flask import jsonify, make_response, request
+from flask_mysqldb import MySQL
+import bcrypt
+
+
+class TaskList(Resource):
+    def __init__(self, **kwargs):
+        self.mysql = kwargs["sql"]
+        self.cursor = self.mysql.connection.cursor()
+
+    def get(self, user_id):
+        self.cursor.execute(f"SELECT id, `name`, due_date FROM task WHERE user_id='{user_id}'")
+        details = self.cursor.fetchall()
+        detail_list = list(details)
+        self.cursor.close()
+        return make_response(jsonify({"response": "success", "tasks": detail_list}), 200)
+
