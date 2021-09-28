e714b4a3d01dc22e68c19b37895f5844b7982d19
Issac Abraham <issaca32@gmail.com>
2021-09-28 18:02:10 +0100

    handles GET request and fixes mySQL connection issues.

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 from flask_restful import Api
-from endpoints.User import User
 import yaml
-
+from endpoints.User import User
 app = Flask(__name__)
+api = Api(app)
 db = yaml.load(open("endpoints/config.yaml"))
 app.config["MYSQL_HOST"] = db['mysql_host']
 app.config["MYSQL_USER"] = db['mysql_user']
 app.config["MYSQL_PASSWORD"] = db['mysql_password']
 app.config["MYSQL_DB"] = db['mysql_db']
-api = Api(app)
-api.add_resource(User, '/api/v1/user/<string:username>', endpoint='get-user', methods=['GET'])
-api.add_resource(User, '/api/v1/user/create', endpoint='create-user', methods=['POST'], resource_class_kwargs={'app': app})
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+api.add_resource(User, '/api/v1/user/<string:email>', endpoint='get-user', methods=['GET'], resource_class_kwargs={"mysql": mysql})
+api.add_resource(User, '/api/v1/user/create', endpoint='create-user', methods=['POST'], resource_class_kwargs={"mysql": mysql})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- endpoints/User.py
+++ endpoints/User.py
@@ -1,16 +1,32 @@
+import yaml
 from flask_restful import Resource
 from flask import jsonify, make_response, request
 from flask_mysqldb import MySQL
-
-
+import bcrypt
 
 
 class User(Resource):
     def __init__(self, **kwargs):
-        self.mysql = MySQL(kwargs['app'])
+        self.mysql = kwargs["sql"]
+        self.cursor = self.mysql.connection.cursor()
 
-    def get(self, username):
-        return make_response(jsonify({"message": "hello"}), 200)
+    def get(self, email):
+        password = request.form["password"]
+        self.cursor.execute(f"SELECT password FROM user WHERE email='{email}'")
+        hashed_password = self.cursor.fetchone()["password"]
+        first_name = ""
+        last_name = ""
+        id = -1
+        if bcrypt.checkpw(password.encode("utf-8"),hashed_password.encode("utf-8")):
+            self.cursor.execute(f"SELECT id, first_name, last_name FROM user WHERE email='{email}'")
+            details = self.cursor.fetchone()
+            first_name = details["first_name"]
+            last_name = details["last_name"]
+            id = details["id"]
+        self.cursor.close()
+        return make_response(jsonify({"response": "success", "match": bcrypt.checkpw(password.encode("utf-8"),
+                                                                                    hashed_password.encode(
+                                                                                        "utf-8")), "first_name": first_name, "last_name": last_name, "email": email, "id": id}), 200)
 
     def post(self):
         for field in ['first_name', 'last_name', 'email', 'password']:
@@ -20,8 +36,9 @@ class User(Resource):
         r_lastname = request.form["last_name"]
         r_email = request.form["email"]
         r_password = request.form["password"]
-        cursor = self.mysql.connection.cursor()
-        cursor.execute(f"INSERT INTO user(first_name, last_name, email, password) VALUES ('{r_firstname}', '{r_lastname}', '{r_email}', '{r_password}');")
+        r_password_hashed = bcrypt.hashpw(r_password.encode('utf-8'), bcrypt.gensalt()).decode("utf-8")
+        self.cursor.execute(
+            f"INSERT INTO user(first_name, last_name, email, password) VALUES ('{r_firstname}', '{r_lastname}','{r_email}', '{r_password_hashed}');")
         self.mysql.connection.commit()
-        cursor.close()
+        self.cursor.close()
         return make_response(jsonify({"response": "success"}), 200)
