f6906bb01e15747e0b86d891169059fd28dbeac9
Issac Abraham <issaca32@gmail.com>
2021-09-28 10:07:08 +0100

    handles User POST request

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_restful import Api
 from endpoints.User import User
+import yaml
 
 app = Flask(__name__)
+db = yaml.load(open("endpoints/config.yaml"))
+app.config["MYSQL_HOST"] = db['mysql_host']
+app.config["MYSQL_USER"] = db['mysql_user']
+app.config["MYSQL_PASSWORD"] = db['mysql_password']
+app.config["MYSQL_DB"] = db['mysql_db']
 api = Api(app)
-
 api.add_resource(User, '/api/v1/user/<string:username>', endpoint='get-user', methods=['GET'])
-api.add_resource(User, '/api/v1/user/create', endpoint='create-user', methods=['POST'])
+api.add_resource(User, '/api/v1/user/create', endpoint='create-user', methods=['POST'], resource_class_kwargs={'app': app})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- endpoints/User.py
+++ endpoints/User.py
@@ -1,14 +1,27 @@
 from flask_restful import Resource
 from flask import jsonify, make_response, request
+from flask_mysqldb import MySQL
+
+
 
 
 class User(Resource):
+    def __init__(self, **kwargs):
+        self.mysql = MySQL(kwargs['app'])
+
     def get(self, username):
         return make_response(jsonify({"message": "hello"}), 200)
 
     def post(self):
-        r_username = request.form["username"]
-        return make_response(jsonify({"message": r_username}), 200)
-
-
-
+        for field in ['first_name', 'last_name', 'email', 'password']:
+            if field not in request.form:
+                return make_response(jsonify({"response": f"missing {field}"}), 200)
+        r_firstname = request.form["first_name"]
+        r_lastname = request.form["last_name"]
+        r_email = request.form["email"]
+        r_password = request.form["password"]
+        cursor = self.mysql.connection.cursor()
+        cursor.execute(f"INSERT INTO user(first_name, last_name, email, password) VALUES ('{r_firstname}', '{r_lastname}', '{r_email}', '{r_password}');")
+        self.mysql.connection.commit()
+        cursor.close()
+        return make_response(jsonify({"response": "success"}), 200)
